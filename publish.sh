342d140de4d097064eac5951c93f09c9a6e23ec6
RJ Bergeron <rbergero@gmail.com>
2018-05-17 17:42:42 -0400

    incorporate CA installation into base docker images.

--- publish.sh
+++ publish.sh
@@ -2,13 +2,11 @@
 
 ts=$(date +%s)
 
-for img in $(docker images "build/*" --format "{{.Repository}}") ; do
-  dist_version="${img##build/}"
-  version="${dist_version##*-}"
-  dist="${dist_version%-${version}}"
-  docker tag "${img}" "${DOCKER_SINK}/${dist}:${version}"
-  docker tag "${img}" "${DOCKER_SINK}/${dist}:${version}.${ts}"
-  docker push "${DOCKER_SINK}/${dist}:${version}"
-  docker push "${DOCKER_SINK}/${dist}:${version}.${ts}"
+for img in $(docker images "final/*" --format "{{.Repository}}:{{.Tag}}") ; do
+  dest="${img#final/}"
+  docker tag "${img}" "${DOCKER_SINK}/${dest}"
+  docker tag "${img}" "${DOCKER_SINK}/${dest}.${ts}"
+  docker push "${DOCKER_SINK}/${dest}"
+  docker push "${DOCKER_SINK}/${dest}.${ts}"
 done
 
